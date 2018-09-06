4b02dadc739fb3a9a0fa925a8b18d48c8f49b93e
Alex <iormundr@gmail.com>
2018-09-06 15:00:02 -0400

    Added single file upload functionality

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -11,7 +11,6 @@ def fetch_buckets_from_s3():
                 print(bucket.name)
         return bucket_list
 
-
 def upload_recursively_to_s3(my_bucket,PATH):
         bucket_to_upload = s3.Bucket(my_bucket)
         for root,dirs,files in os.walk(PATH):
@@ -22,10 +21,27 @@ def upload_recursively_to_s3(my_bucket,PATH):
                                 print("Uploading  ", os.path.join(root,file))
 
 def verify_file_exist_for_upload(PATH):
+        bool_list = []
         for i in range(len(PATH)):
-                if not os.path.exists(PATH[i]):
-                        print("File or directiry does not exist in the specified PATH")
+                if os.path.isdir(PATH[i]):
+                        bool_list.append(False)
+                        pass
+                elif os.path.isfile(PATH[i]):
+                        bool_list.append(True)
+                else:
+                        print
+                        print("Specified input is neither file nor directory, check your input\n")
                         exit()
+        return bool_list
+
+def upload_single_files(my_bucket,PATH,isFile):
+        print
+        print("Printing standalone files\n")
+        for bool_value in range(len(PATH)):
+                if isFile[bool_value]:
+                        s3.Bucket(my_bucket).put_object(Key=PATH[bool_value],Body=PATH[bool_value])
+                        print("Uploading  ", PATH[bool_value])
+
 
 if __name__ == "__main__":
         s3 = boto3.resource('s3')
@@ -34,14 +50,16 @@ if __name__ == "__main__":
                 exit()
         elif len(sys.argv) >= 2:
                 PATH = sys.argv[1:]
-                verify_file_exist_for_upload(PATH)
-        print("")
+                return_code = verify_file_exist_for_upload(PATH)
+                print(return_code)
+        print
         bucket_list = fetch_buckets_from_s3()
-        print("")
+        print
         my_bucket = raw_input("Please select bucket name to upload to: ")
-        print("")
+        print
         if my_bucket not in bucket_list:
                 print("Bucket name was not entered correctly")
                 exit()
         for i in range(len(PATH)):
                 upload_recursively_to_s3(my_bucket,PATH[i])
+        upload_single_files(my_bucket,PATH,return_code)
